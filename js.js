28dfbf5f6fb871ae95c9bfb6e9778c59c4e35d49
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2018-01-28 07:47:26 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,153 @@
+(function () {
+    'use strict';
+
+    let arrayOfUnicode = [
+        ['', '', '', '', '', '',
+            '\ud83c\udca6',
+            '\ud83c\udca7',
+            '\ud83c\udca8',
+            '\ud83c\udca9',
+            '\ud83c\udcaa',
+            '\ud83c\udcab',
+            '\ud83c\udcad',
+            '\ud83c\udcae',
+            '\ud83c\udca1'],
+        ['', '', '', '', '', '',
+            '\ud83c\udcb6',
+            '\ud83c\udcb7',
+            '\ud83c\udcb8',
+            '\ud83c\udcb9',
+            '\ud83c\udcba',
+            '\ud83c\udcbb',
+            '\ud83c\udcbd',
+            '\ud83c\udcbe',
+            '\ud83c\udcb1'],
+        ['', '', '', '', '', '',
+            '\ud83c\udcc6',
+            '\ud83c\udcc7',
+            '\ud83c\udcc8',
+            '\ud83c\udcc9',
+            '\ud83c\udcca',
+            '\ud83c\udccb',
+            '\ud83c\udccd',
+            '\ud83c\udcce',
+            '\ud83c\udcc1'],
+        ['', '', '', '', '', '',
+            '\ud83c\udcd6',
+            '\ud83c\udcd7',
+            '\ud83c\udcd8',
+            '\ud83c\udcd9',
+            '\ud83c\udcda',
+            '\ud83c\udcdb',
+            '\ud83c\udcdd',
+            '\ud83c\udcde',
+            '\ud83c\udcd1']];
+    let nameOfSuit = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+    console.log(arrayOfUnicode);
+    let Card = function (number, suit) {
+        this.number = number;
+        this.suit = suit;
+    };
+
+    function defineTrump() {
+        return Math.floor(Math.random() * 4);
+    };
+
+    let cardsArray = [];
+    let playersPacksOfCards = [];
+
+    function createPackOfCardsArray() {
+        for (let suit = 0; suit <= 3; suit++) {
+            for (let number = 6; number <= 14; number++) {
+                let card = new Card(number, suit);
+                cardsArray.push(card);
+            }
+        }
+        return cardsArray;
+    };
+
+    function mixDeck(cardsArray) {
+        return cardsArray.sort(function () {
+            return (Math.random() < 0.5 ? 1 : -1)
+        });
+    };
+
+    function distributePackOfCards(cardArray) {
+        let firstPlayerCardArrays = [];
+        let secondPlayerCardArrays = [];
+        for (let i = 0; i < cardArray.length / 2; i++) {
+            firstPlayerCardArrays[i] = cardArray[i];
+            secondPlayerCardArrays[i] = cardArray[i + 18];
+        }
+        ;
+        playersPacksOfCards = [firstPlayerCardArrays, secondPlayerCardArrays];
+        return playersPacksOfCards;
+    };
+
+    createPackOfCardsArray();
+    mixDeck(cardsArray);
+    distributePackOfCards(cardsArray);
+    let firstPlayerCardArrays = playersPacksOfCards[0];
+    let secondPlayerCardArrays = playersPacksOfCards[1];
+    let trump = defineTrump();
+    let insertTrump = document.getElementById('trump');
+    insertTrump.innerHTML = nameOfSuit[trump];
+    let firstPlayerScore = document.getElementById('firstScore');
+    let secondPlayerScore = document.getElementById('secondScore');
+    let tableOfResults = document.getElementById('tableResult');
+    let nameWinner = document.getElementById('nameOfWinner');
+let countOfSteps=0;
+
+    function* gameStart() {
+
+    let firstPlayerCard = firstPlayerCardArrays.pop();
+    let secondPlayerCard = secondPlayerCardArrays.pop();
+    let newRow = document.createElement('tr');
+    let newCellFirst = document.createElement('td');
+    let newCellSecond = document.createElement('td');
+    tableOfResults.appendChild(newRow);
+    newCellFirst.innerHTML = arrayOfUnicode[firstPlayerCard.suit][firstPlayerCard.number];
+    newCellSecond.innerHTML = arrayOfUnicode[secondPlayerCard.suit][secondPlayerCard.number];
+    newRow.appendChild(newCellFirst);
+    newRow.appendChild(newCellSecond);
+    if (firstPlayerCard.suit === trump && secondPlayerCard.suit === trump) {
+        if (firstPlayerCard.number > secondPlayerCard.number) {
+            firstPlayerScore.innerHTML++;
+        } else {
+            secondPlayerScore.innerHTML++;
+        }
+    }
+    if (firstPlayerCard.suit === trump && secondPlayerCard.suit !== trump) {
+        firstPlayerScore.innerHTML++;
+    }
+    if (secondPlayerCard.suit === trump && firstPlayerCard.suit !== trump) {
+        secondPlayerScore.innerHTML++;
+    }
+    if (firstPlayerCard.suit !== trump && secondPlayerCard.suit !== trump) {
+        if (firstPlayerCard.number > secondPlayerCard.number) {
+            firstPlayerScore.innerHTML++;
+        } else {
+            secondPlayerScore.innerHTML++;
+        }
+    }
+    countOfSteps++;
+    yield;
+    }
+
+    document.addEventListener('click', function () {
+        if (countOfSteps<18) {
+         return gameStart().next();}
+         else {
+            if (+firstPlayerScore.innerHTML > +secondPlayerScore.innerHTML)
+            {nameWinner.innerHTML = 'Петя';}
+            if (+firstPlayerScore.innerHTML < +secondPlayerScore.innerHTML)
+            {   nameWinner.innerHTML = 'Вася';}
+            if (+firstPlayerScore.innerHTML === +secondPlayerScore.innerHTML)
+            {   nameWinner.innerHTML = 'Ничья';}
+        }
+    });
+
+
+
+
+}());
